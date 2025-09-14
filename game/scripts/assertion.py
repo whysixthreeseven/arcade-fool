2d2a7267c4e6aed39e18155b4682ea0b5e4a74c5
Andrey Vostokov <whysixthreeseven@gmail.com>
2025-09-14 13:33:52 +0800

    Add assertion utility functions
    Introduced three utility functions: assert_value_is_valid_type, assert_value_is_default, and assert_value_in_range. These functions provide type checking, default value checking, and range checking with optional error raising for validation purposes.

---
+++ game/scripts/assertion.py
@@ -0,0 +1,62 @@
+# Typing import:
+from typing import Any
+
+
+def assert_value_is_valid_type(check_value: Any, 
+                               check_type_list: tuple[type, ...], 
+                               raise_error: bool = True
+                               ) -> bool:
+    """
+    TODO: Create a docstring.
+    """
+
+    # Asserting value is within the expected type list:
+    assert_eval: bool = isinstance(check_value, check_type_list)
+    assert_error: str = f"Value {check_value=} is not expected type ({check_type_list=})."
+
+    # Raising error (if enabled):
+    if raise_error:
+        assert assert_eval, assert_error
+
+    # Returning (if reached):
+    return assert_eval
+
+
+def assert_value_is_default(check_value: Any,
+                            check_list: tuple[Any, ...],
+                            raise_error: bool = True
+                            ) -> bool:
+    """
+    TODO: Create a docstring.
+    """
+
+    # Asserting value is within the expected type list:
+    assert_eval: bool = check_value in check_list
+    assert_error: str = f"Value {check_value=} is not expected as default ({check_list=})."
+
+    # Raising error (if enabled):
+    if raise_error:
+        assert assert_eval, assert_error
+
+    # Returning (if reached):
+    return assert_eval
+
+
+def assert_value_in_range(check_value: int | float,
+                          check_range: range,
+                          raise_error: bool = True
+                          ) -> bool:
+    """
+    TODO: Create a docstring.
+    """
+
+    # Asserting value is within the expected range:
+    assert_eval: bool = int(check_value) in check_range
+    assert_error: str = f"Value {check_value=} is not in expected range ({check_range=})."
+
+    # Raising error (if enabled):
+    if raise_error:
+        assert assert_eval, assert_error
+
+    # Returning (if reached):
+    return assert_eval
