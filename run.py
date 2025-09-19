4776a77c1db7002f08c1d6f9400c362897949f8e
Andrey Vostokov <whysixthreeseven@gmail.com>
2025-09-19 13:20:42 +0800

    Add run.py entry point and remove test scripts
    Introduced run.py as the main entry point for running the Gameshell application using the Arcade library. Removed test.py and test2.py, consolidating functionality into the new run.py script.

---
+++ run.py
@@ -0,0 +1,27 @@
+# Arcade library import
+import arcade
+
+# Gameshell instance import:
+from game.gameshell import Gameshell
+
+
+"""
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+MAIN / RUN METHOD BLOCK
+
+"""
+
+
+def run():
+    """
+    TODO: Create a docstring.
+    """
+
+    # Creating gameshell instance and running arcade:
+    gameshell = Gameshell()
+    arcade.run() 
+
+
+if __name__ == "__main__":
+    run()
+
--- test.py
+++
@@ -1,8 +0,0 @@
-import arcade
-
-
-from game.gameshell import Gameshell
-
-
-gameshell = Gameshell()
-arcade.run() 
