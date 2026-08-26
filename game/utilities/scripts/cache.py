bbc2a925392313c12ae1be6b87c2fc9b8e50faef
Andrey Vostokov <whysixthreeseven@gmail.com>
2026-08-26 22:07:59 +0800

    Added wake up function

--- game/utilities/scripts/cache.py
+++ game/utilities/scripts/cache.py
@@ -1,3 +1,8 @@
+# Cache- and other related libraries and modules:
+from functools import cached_property
+import inspect
+
+
 def clear_cached_property(target_object: object, target_attribute: str) -> None:
     if hasattr(target_object, target_attribute):
         delattr(target_object, target_attribute)
@@ -10,3 +15,8 @@ def clear_cached_property_list(target_object: object, target_attribute_list: tup
             target_attribute,
             )
 
+
+def refresh_object(target_object: object) -> None:
+    for attribute_name, attribute_value in inspect.getmembers(target_object.__class__):
+        if isinstance(attribute_value, cached_property):
+            getattr(target_object, attribute_name)
