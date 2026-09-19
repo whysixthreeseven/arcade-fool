7a7d9635d03647649eab2dc6bab422ea9073f504
Andrey Vostokov <whysixthreeseven@gmail.com>
2026-09-19 16:56:26 +0800

    Updated date

--- game/settings.py
+++ game/settings.py
@@ -9,7 +9,7 @@ class __SETTINGS:
     APP_DESCRIPTION: str = "Card game 'Fool' created with Python and Arcade library"
     APP_VERSION: str = "0.0.1R"
     APP_STARTED: str = "2026.09.02"
-    APP_UPDATED: str = "2026.09.14"
+    APP_UPDATED: str = "2026.09.19"
     APP_AUTHOR: str = "Andrey Vostokov"
 
     # Root directory settings:    
