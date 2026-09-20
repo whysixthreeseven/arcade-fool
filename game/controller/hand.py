58b320e807a8d495c7c8800fb331d8c00b1b208d
Andrey Vostokov <whysixthreeseven@gmail.com>
2026-09-20 13:53:58 +0800

    Created Hand controller draft

---
+++ game/controller/hand.py
@@ -0,0 +1,16 @@
+
+
+
+
+class Hand:
+    
+    def __init__(self) -> None:
+        
+        # Owner attributes:
+        self.__owner: str = None
+        
+        # Container attributes:
+        self.__container: list = []
+        
+    
+    
