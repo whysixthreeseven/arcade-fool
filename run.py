d381a9da075b7f9a17de5a8d1f59b8a85a875466
Andrey Vostokov <whysixthreeseven@gmail.com>
2026-09-22 14:22:12 +0800

    Created main entry run() function

---
+++ run.py
@@ -0,0 +1,25 @@
+# Arcade library:
+import arcade
+
+# Gameshell class instance:
+from game.gameshell import Gameshell
+
+
+def run() -> None:
+    """
+    Runs the game. Main entry point.
+    
+    Note that game runs with fullscreen and resizable options turned off. Ensure your screen area allows
+    for this. Otherwise parts of the game screen may be obscured or invisible.
+    """
+    
+    # Creating gameshell instance and running a quick set-up:
+    gameshell: Gameshell = Gameshell()
+    
+    # Starting arcade loop:
+    arcade.run()
+    
+
+if __name__ == "__main__":
+    run()
+
